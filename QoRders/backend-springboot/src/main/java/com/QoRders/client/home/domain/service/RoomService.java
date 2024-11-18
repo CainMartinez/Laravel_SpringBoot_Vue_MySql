c3aba8c533006c2cae637ced1ef0ebb0b4cb50ba
Cain <cainmartinez2@hotmail.com>
2024-11-18 14:18:21 +0100

    Refactor RoomService: convert to interface and implement RoomServiceImpl for improved structure

--- QoRders/backend-springboot/src/main/java/com/QoRders/client/home/domain/service/RoomService.java
+++ QoRders/backend-springboot/src/main/java/com/QoRders/client/home/domain/service/RoomService.java
@@ -1,31 +1,11 @@
 package com.QoRders.client.home.domain.service;
 
 import com.QoRders.client.home.domain.model.Room;
-import com.QoRders.client.home.domain.repository.RoomRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class RoomService {
-
-    private final RoomRepository roomRepository;
-
-    // Inyección de dependencias a través del constructor
-    public RoomService(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
-    }
-
-    // Listar todas las salas
-    public List<Room> listAll() {
-        List<Room> rooms = roomRepository.findAll();
-        System.out.println("Rooms found: " + rooms); // Log para depurar
-        return rooms;
-    }
-
-    // Listar una sala por su slug
-    public Optional<Room> listOneBySlug(String slug) {
-        return roomRepository.findByRoomSlug(slug);
-    }
+public interface RoomService {
+    List<Room> listAll();
+    Optional<Room> listOneBySlug(String slug);
 }
