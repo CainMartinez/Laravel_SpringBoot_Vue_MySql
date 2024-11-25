0d98d2b7838dc2b07784c9ba2bcd308bf6b31eab
Cain <cainmartinez2@hotmail.com>
2024-11-25 15:39:18 +0100

    Vue: Add ProductsService for product management; implement create, update, enable, and disable product methods; export RoomsService

---
+++ QoRders/frontend-vue/src/services/admin/ProductsService.js
@@ -0,0 +1,46 @@
+import { api_laravel } from "../api";
+
+const ProductsService = {
+    async createProduct() {
+        try {
+            const response = await api_laravel.post('/products');
+            console.log("Productos obtenidos:", response.data);
+            return response.data;
+        } catch (error) {
+            console.error("Error al obtener los productos:", error);
+            throw error;
+        }
+    },
+
+    async updateProduct(slug, data) {
+            try {
+                const response = await api_laravel.put(`/products/${slug}`, data);
+                return response.data;
+            } catch (error) {
+                console.error(`Error al actualizar el producto con slug: ${slug}`, error);
+                throw error;
+            }
+        },
+
+    async unableProduct(slug) {
+        try {
+            const response = await api_laravel.put(`/products/${slug}/unable`);
+            return response.data;
+        } catch (error) {
+            console.error(`Error al deshabilitar el producto con slug: ${slug}`, error);
+            throw error;
+        }
+    },
+
+    async enableProduct(slug) {
+        try {
+            const response = await api_laravel.put(`/products/${slug}/enable`);
+            return response.data;
+        } catch (error) {
+            console.error(`Error al habilitar el producto con slug: ${slug}`, error);
+            throw error;
+        }
+    }
+};
+
+export default ProductsService;
--- QoRders/frontend-vue/src/services/admin/RoomsService.js
+++ QoRders/frontend-vue/src/services/admin/RoomsService.js
@@ -42,3 +42,5 @@ const RoomsService = {
         }
     }
 };
+
+export default RoomsService;
