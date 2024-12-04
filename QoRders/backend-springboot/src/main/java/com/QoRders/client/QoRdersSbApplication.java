b5907ee8add38a4db7213d049b4bc4f8a6d85c56
Cain <cainmartinez2@hotmail.com>
2024-12-04 13:58:12 +0100

    Spring Boot: Add logging configuration and redirect output to log files; create logback configuration for file and console logging.

--- QoRders/backend-springboot/src/main/java/com/QoRders/client/QoRdersSbApplication.java
+++ QoRders/backend-springboot/src/main/java/com/QoRders/client/QoRdersSbApplication.java
@@ -1,11 +1,15 @@
 package com.QoRders.client;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.QoRders.client")
 public class QoRdersSbApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        System.setOut(new PrintStream(new FileOutputStream("logs/output.log", true)));
         SpringApplication.run(QoRdersSbApplication.class, args);
     }
 }
