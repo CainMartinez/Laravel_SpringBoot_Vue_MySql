144de0f800bd66b563ded74abb82bea553f3a9a1
Cain <cainmartinez2@hotmail.com>
2024-12-06 16:58:18 +0100

    Spring Boot: Add Redis support with configuration and service implementation; update Docker Compose to remove unused JWT settings

---
+++ QoRders/backend-springboot/src/main/java/com/QoRders/client/redis/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.QoRders.client.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Serializadores para las claves y valores
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+}
