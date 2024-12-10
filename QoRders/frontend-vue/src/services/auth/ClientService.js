efcf67a40cb81fa50c26fabce4f831b7b2ca1bbe
Angela Torro <a.torroballester@gmail.com>
2024-12-10 15:59:51 +0100

    Vue: Add ClientService for authentication: Implement login, registration, and user management methods with error handling.

---
+++ QoRders/frontend-vue/src/services/auth/ClientService.js
@@ -0,0 +1,62 @@
+import { api_spring } from "../api";
+
+const ClientService = {
+    async login(email, password) {
+        try {
+            console.log("ClientService login");
+            console.log({ email, password });
+            const response = await api_spring.post('/auth/login', {
+                role,
+                email,
+                password,
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Error al iniciar sesión:", error);
+            throw error;
+        }
+    },
+
+    async register(firstName, lastName, email, password, repeatPassword, role) {
+        try {
+            console.log("ClientService register");
+            console.log({ firstName, lastName, email, password, repeatPassword, role });
+            const response = await api_spring.post('/auth/register', {
+                role,
+                firstName,
+                lastName,
+                email,
+                password,
+                repeatPassword,
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Error al registrar:", error);
+            throw error;
+        }
+    },
+
+    async getCurrentUser(token) {
+        try {
+            const response = await api_spring.get('/client', {
+                headers: { Authorization: `Bearer ${token}` },
+            }); 
+            return response.data;
+        } catch (error) {
+            console.error("Error al obtener el usuario actual:", error);
+            throw error;
+        }
+    },
+
+    async logout() {
+        try {
+            const response = await api_spring.post('/auth/logout');
+            return response.data;
+        } catch (error) {
+            console.error("Error al cerrar sesión:", error);
+            throw error;
+        }
+    }
+};
+
+export default ClientService;
