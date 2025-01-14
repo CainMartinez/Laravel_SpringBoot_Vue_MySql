5ba08faf91c8cd304fefb75cb376b3e9cc78f685
Cain <cainmartinez2@hotmail.com>
2025-01-14 17:19:14 +0100

    Add Maven Javadoc plugin and enhance main application class documentation

--- QoRders/backend-springboot/src/main/java/com/QoRders/client/QoRdersSbApplication.java
+++ QoRders/backend-springboot/src/main/java/com/QoRders/client/QoRdersSbApplication.java
@@ -2,9 +2,19 @@ package com.QoRders.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Clase principal de la aplicación.
+ *
+ * @author John
+ * @version 1.0
+ */
 @SpringBootApplication(scanBasePackages = "com.QoRders.client")
 public class QoRdersSbApplication {
+    /**
+     * Método principal de la aplicación.
+     *
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args){
         SpringApplication.run(QoRdersSbApplication.class, args);
     }
