f0ecbeefad526cd86a60b2c7935d8665a2c4357a
Cain <cainmartinez2@hotmail.com>
2025-03-03 23:05:31 +0100

    Refactor Calendar component: simplify date selection logic and improve styling for available and disabled days

--- QoRders/frontend-vue/src/components/Calendar.vue
+++ QoRders/frontend-vue/src/components/Calendar.vue
@@ -2,47 +2,28 @@
     <div>
         <DatePicker v-model="selectedDate" :dateFormat="'dd-mm-yy'" :inline="true">
             <template #date="slotProps">
-                <div v-if="checkGreenDay(slotProps.date)" class="availableDay"
-                    @click="selectDay(slotProps.date, false)">
-                    {{ slotProps.date.day }}</div>
-                <div v-tooltip.top="'Hay disponibilidad en otras salas'" v-else-if="checkYellowDay(slotProps.date)"
-                    class="otherRoom" @click="selectDay('', true)">
-                    {{ slotProps.date.day }}</div>
-                <div v-tooltip.top="'No hay disponibilidad para hoy'" v-else-if="checkRedDay(slotProps.date)"
-                    class="unavailable" @click="selectDay('', true)">{{
-                        slotProps.date.day }}</div>
-                <div v-else>{{ slotProps.date.day }}</div>
+                <div :class="{'selectable-day': isFutureDate(slotProps.date), 'disabled-day': !isFutureDate(slotProps.date)}"
+                    @click="selectDay(slotProps.date)">
+                    {{ slotProps.date.day }}
+                </div>
             </template>
         </DatePicker>
     </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import { useStore } from 'vuex';
-
-const props = defineProps({
-    roomCapacity: Number,
-    selectedRoom: String,
-    selectedShift: String,
-    selectedPeople: Number
-});
+import { ref } from 'vue';
 
 const emit = defineEmits(['update:selectedDay']);
 const selectedDate = ref(null);
-const store = useStore();
-const shifts = computed(() => store.getters['storeReservation/getShifts'].data);
 
+// Función para convertir la fecha a formato YYYY-MM-DD
 const translateDate = (date) => {
     const padZero = (num) => (num < 10 ? '0' + num : num);
-
-    const year = date.year;
-    const month = padZero(date.month + 1);
-    const day = padZero(date.day);
-
-    return `${year}-${month}-${day}`;
+    return `${date.year}-${padZero(date.month + 1)}-${padZero(date.day)}`;
 };
 
+// Obtener la fecha actual en formato YYYY-MM-DD
 const today = new Date();
 const todayString = translateDate({
     year: today.getFullYear(),
@@ -50,62 +31,22 @@ const todayString = translateDate({
     day: today.getDate()
 });
 
-const checkGreenDay = (date) => {
-    if (translateDate(date) < todayString) return false;
-
-    let dayShifts = shifts.value.filter(s => s.shiftDate === translateDate(date));
-    if (dayShifts.length === 0) return false;
-    let shiftSelected = dayShifts.find(s => s.shiftType === props.selectedShift);
-    let roomAvailability = props.roomCapacity - shiftSelected.roomShifts.find(r => r.roomName === props.selectedRoom).reservedCapacity;
-    if (roomAvailability >= props.selectedPeople) return true;
-    return false;
+// Verifica si la fecha es hoy o en el futuro
+const isFutureDate = (date) => {
+    return translateDate(date) >= todayString;
 };
 
-const checkYellowDay = (date) => {
-    if (translateDate(date) < todayString) return false;
-
-    let dayShifts = shifts.value.filter(s => s.shiftDate === translateDate(date));
-    if (dayShifts.length === 0) return false;
-    let shiftSelected = dayShifts.find(s => s.shiftType === props.selectedShift);
-
-    return shiftSelected.roomShifts.some(r => {
-        let roomAvailability = props.roomCapacity - r.reservedCapacity;
-        if (roomAvailability >= props.selectedPeople) {
-            return true;
-        }
-        return false;
-    });
-};
-
-const checkRedDay = (date) => {
-    if (translateDate(date) < todayString) return false;
-
-    let dayShifts = shifts.value.filter(s => s.shiftDate === translateDate(date));
-    if (dayShifts.length === 0) return false;
-    let shiftSelected = dayShifts.find(s => s.shiftType === props.selectedShift);
-    return shiftSelected.roomShifts.some(r => {
-        let roomAvailability = props.roomCapacity - r.reservedCapacity;
-        if (roomAvailability >= props.selectedPeople) {
-            return false;
-        }
-
-        return true;
-    });
-};
-
-const selectDay = (date, isDisabled) => {
+// Emitir la fecha seleccionada solo si es hoy o en el futuro
+const selectDay = (date) => {
     const translatedDate = translateDate(date);
-    const dayInfo = {
-        day: translatedDate,
-        isDisabled: isDisabled
-    };
-    emit('update:selectedDay', dayInfo);
+    if (isFutureDate(date)) {
+        emit('update:selectedDay', { day: translatedDate, isDisabled: false });
+    }
 };
-
 </script>
 
 <style scoped>
-.availableDay {
+.selectable-day {
     background-color: #80cc80;
     border-radius: 50%;
     width: 30px;
@@ -113,20 +54,14 @@ const selectDay = (date, isDisabled) => {
     display: flex;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
 }
 
-.otherRoom {
-    background-color: #ecec60;
-    border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: not-allowed;
+.selectable-day:hover {
+    background-color: #66a366;
 }
 
-.unavailable {
+.disabled-day {
     background-color: #e48383;
     border-radius: 50%;
     width: 30px;
@@ -134,6 +69,8 @@ const selectDay = (date, isDisabled) => {
     display: flex;
     align-items: center;
     justify-content: center;
+    color: white;
     cursor: not-allowed;
+    opacity: 0.6;
 }
 </style>
